b8cbfdec9caaf9685f50d2962f5c269dbf5c1b10
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2025-08-31 10:59:28 -0600

    "Pruebas de impresion al tener archivo.rs de entrada"

--- main.cpp
+++ main.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
-//#include <FlexLexer.h>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include "cmake-build-debug/parser.hpp" // Generado por Bison
+
+//Funciones del Lexer y Parser generados por Bison y Flex
+extern int yylex();
+extern char* yytext;
+extern FILE* yyin;
 extern int yyparse();
 
 using namespace std;
 
 int main() {
-    std::cout << "Ingresa codigo para analizar:\n";
+    const char* file = "../RustFile.rs";
+    yyin = fopen(file, "r");
+    if (!yyin) {
+        perror("Error al abrir archivo");
+        return 1;
+    }
+
+    cout << "=== Analisis Lexico ===\n";
+    int token;
+    string tokenType;
+    while ((token = yylex()) != 0) {
+        switch(token) {
+            case PRINT:  tokenType= "PRINT"; break;
+            case STRING: tokenType="STRING"; break;
+            default:     tokenType= "UNKNOWN"; break;
+        }
+        cout << "Token de Salida: " << tokenType <<" - " << token << " Texto de Entrada: " << yytext << "\n";
+
+    }
+
+    fseek(yyin, 0, SEEK_SET);
+
+    cout << "=== Analisis Sintactico ===\n";
     yyparse();
+
+    fclose(yyin);
     return 0;
 }
